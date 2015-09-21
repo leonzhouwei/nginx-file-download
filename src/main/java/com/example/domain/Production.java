049d850042838298b81c5259ba97b29c02bd37b6
wei.zhou <wei.zhou@autonavi.com>
2015-09-21 17:38:19 +0800

    delete unused variables

--- src/main/java/com/example/domain/Production.java
+++ src/main/java/com/example/domain/Production.java
@@ -4,7 +4,6 @@ public class Production extends Base {
 	
 	private Long id;
 	private String description;
-	private Long ret;
 	
 	public Long getId() {
 		return id;
@@ -20,14 +19,6 @@ public class Production extends Base {
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	public Long getRet() {
-		return ret;
-	}
-
-	public void setRet(Long ret) {
-		this.ret = ret;
 	}
 
 }
