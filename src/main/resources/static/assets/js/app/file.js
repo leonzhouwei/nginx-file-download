eba1aa207762755101f81936dc663d315a6eb21b
wei.zhou <wei.zhou@autonavi.com>
2015-10-19 10:44:37 +0800

    table can be filled according to the data from HTTP API

--- src/main/resources/static/assets/js/app/file.js
+++ src/main/resources/static/assets/js/app/file.js
@@ -2,6 +2,10 @@ $(function() {
 	$.get("/api/files", function(result) {
 		for (var i = 0, len = result.length; i < len; ++i) {
 			console.log(result[i]);
+			var table = $('#table');
+			var newRow = '<tr><td>' + result[i]['id'] + '</td><td>'
+					+ result[i]['name'] + '</td></tr>';
+			$('#table tr:last').after(newRow);
 		}
 	});
 });
