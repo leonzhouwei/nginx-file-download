73210ac1c53e9db77a77d45bd7c185c2176054ac
wei.zhou <wei.zhou@autonavi.com>
2015-10-23 16:11:27 +0800

    give different main pages to the administrator and normal users

--- src/main/java/com/example/webgui/WebGuiIndexController.java
+++ src/main/java/com/example/webgui/WebGuiIndexController.java
@@ -1,13 +1,29 @@
 package com.example.webgui;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Account;
+import com.example.filter.LoginInterceptor;
+import com.example.persist.mapper.AccountRMapper;
 
 @Controller
 public class WebGuiIndexController {
 	
+	@Autowired
+	private AccountRMapper accoutRMapper;
+
 	@RequestMapping("/")
-	public String index() {
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		Long id = LoginInterceptor.getAccountId(request);
+		Account account = accoutRMapper.selectById(id);
+		if (account.getIsAdmin()) {
+			return "admin/index";
+		}
 		return "index";
 	}
 
