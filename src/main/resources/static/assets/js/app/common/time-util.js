13c11a9dba4fcbf9dfde26c7f31de0d1aa41bc10
wei.zhou <wei.zhou@autonavi.com>
2015-10-28 16:31:40 +0800

    imporove the performance

--- src/main/resources/static/assets/js/app/common/time-util.js
+++ src/main/resources/static/assets/js/app/common/time-util.js
@@ -1,4 +1,11 @@
 function iso8601ToHuman(iso8601) {
-	var ret = iso8601.replace('T', ' ');
+	var index = iso8601.indexOf('.');
+	var ret = iso8601;
+	if (index > 0) {
+		ret = ret.substring(0, index);
+	}
+	if (ret.indexOf('T') > 0) {
+		ret = ret.replace('T', ' ');
+	}
 	return ret;
 }
