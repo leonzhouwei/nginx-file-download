f6a732b9e35a198b85e839bc7ee093341f68dfae
wei.zhou <wei.zhou@autonavi.com>
2015-10-28 16:49:48 +0800

    DRY

--- src/main/resources/static/assets/js/app/dld-task.js
+++ src/main/resources/static/assets/js/app/dld-task.js
@@ -14,26 +14,15 @@ function initTable(result) {
 		} else {
 			buffer.push('<tr>');
 		}
+		var hhmmss = toHhmmddHuman(elem['timeCostMillis']);
 		buffer.push('<td>', elem['id'], '</td>');
 		buffer.push('<td>');
 		buffer.push('<a href="/files/' + elem['fileId'] + '" target="_blank">',
 				elem['fileId'], '</a>');
 		buffer.push('</td>');
 		buffer.push('<td>', elem['clientIp'], '</td>');
-		buffer.push('<td>', iso8601ToHuman(elem['createdAt']),
-				'</td>');
-		var timeCostSeconds = parseInt(elem['timeCostMillis'] / 1000);
-		var hours = parseInt(timeCostSeconds / 3600);
-		var minutes = parseInt((timeCostSeconds - 3600 * hours) / 60);
-		if (minutes < 10) {
-			minutes = '0' + minutes;	
-		}
-		var seconds = parseInt(timeCostSeconds - 3600 * hours - 60 * minutes);
-		if (seconds < 10) {
-			seconds = '0' + seconds;	
-		}
-		buffer.push('<td style="text-align: right;">', hours, 'h ', minutes,
-				'm ', seconds, 's', '</td>');
+		buffer.push('<td>', iso8601ToHuman(elem['createdAt']), '</td>');
+		buffer.push('<td style="text-align: right;">', hhmmss, '</td>');
 		buffer.push('/<tr>');
 		var newRow = buffer.join('');
 		$('#table tr:last').after(newRow);
