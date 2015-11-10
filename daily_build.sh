231b514c39b82cd203ed3ae23c5cca2f0de4195e
wei.zhou <wei.zhou@autonavi.com>
2015-11-10 11:30:36 +0800

    versionize the auto-built directory for deployment

--- daily_build.sh
+++ daily_build.sh
@@ -10,24 +10,28 @@ JAR_BASE_NAME=nginx-file-download
 
 ################################################################################
 #################### !! DO NOT EDIT THE STATEMENTS BELOW !! ####################
-APP=app
 base_path=$(cd `dirname $0`; pwd)
 cd $base_path
-
 mvn clean
 mvn package -Dmaven.test.skip=true
 
+APP=app
+JAR_FILE_BASE_NAME=$(basename $TARGET/$JAR_BASE_NAME*.jar)
+VERSION=${JAR_FILE_BASE_NAME/$JAR_BASE_NAME-/''}
+VERSION=${VERSION/'.jar'/''}
+BUILD_VERSION=$BUILD/$VERSION
+
 rm -rf $BUILD
-mkdir -p $BUILD/$APP
+mkdir -p $BUILD_VERSION/$APP
 
-cp -r $APP/data $BUILD/$APP/
-cp -r $CONFIG $BUILD/
-cp -r $CUSTOM $BUILD/
+cp -r $APP/data $BUILD_VERSION/$APP/
+cp -r $CONFIG $BUILD_VERSION/
+cp -r $CUSTOM $BUILD_VERSION/
 
-cp *.sh $BUILD/
-#rm $BUILD/build.sh
-rm $BUILD/daily_build.sh
+cp *.sh $BUILD_VERSION/
+#rm $BUILD_VERSION/build.sh
+rm $BUILD_VERSION/daily_build.sh
 
-cp $TARGET/$JAR_BASE_NAME*.jar $BUILD/
+cp $TARGET/$JAR_BASE_NAME*.jar $BUILD_VERSION/
 
 exit 0
