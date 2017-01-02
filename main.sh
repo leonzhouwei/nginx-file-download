b83ad0de86c088c555056f3361610965981fc247
Wei Zhou <leonzhouwei@gmail.com>
2017-01-02 21:18:25 +0800

    refine JVM options

--- main.sh
+++ main.sh
@@ -9,6 +9,6 @@ JAR_BASE_NAME=nginx-file-download
 base_path=$(cd `dirname $0`; pwd)
 cd $base_path
 
-java -jar ./$JAR_BASE_NAME*.jar -server -XX:+UseConcMarkSweepGC  -XX:CMSInitiatingOccupancyFraction=80
+-server -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:MaxTenuringThreshold=6 -XX:+ExplicitGCInvokesConcurrent -XX:+ParallelRefProcEnabled -Xms1024m -Xmx1024m -XX:NewRatio=1 -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -Xloggc:/Users/zhouwei/Desktop/tmp/dev/shm/gc-myapplication.log -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDateStamps -XX:+PrintGCDetails -jar ./$JAR_BASE_NAME*.jar
 
 exit 0
