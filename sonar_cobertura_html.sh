372dc11f49c826f1476584604347c4786f14fd89
Wei Zhou <leonzhouwei@gmail.com>
2017-02-23 15:39:03 +0800

    chore: support cobertura

---
+++ sonar_cobertura_html.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+######################################################################
+base_path=$(cd `dirname $0`; pwd)
+
+cd ${base_path}
+
+# Build the project, run the unit tests and generate the Cobertura report in XML
+# format:
+mvn clean compile
+mvn cobertura:cobertura -Dcobertura.report.format=html
+
+#Analyze the project with SonarQube using Maven:
+mvn sonar:sonar
+
+exit 0
