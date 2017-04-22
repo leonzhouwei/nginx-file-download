4275c07a30969e0d2aa95a6b0994c343896b7d16
Wei Zhou <leonzhouwei@gmail.com>
2017-04-22 21:51:08 +0800

    docs: cobertura works well with sonar

--- cobertura_xml.sh
+++ cobertura_xml.sh
@@ -8,9 +8,6 @@ cd ${base_path}
 # Build the project, run the unit tests and generate the Cobertura report in XML
 # format:
 mvn clean compile
-mvn cobertura:cobertura -Dcobertura.report.format=html
-
-#Analyze the project with SonarQube using Maven:
-mvn sonar:sonar
+mvn cobertura:cobertura -Dcobertura.report.format=xml
 
 exit 0
